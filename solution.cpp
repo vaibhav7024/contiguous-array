3e02da1d5c3952967988ff7695dbe27d4292bdab
vaibhav7024 <vjaiswal7024@gmail.com>
2026-02-11 23:15:09 +0530

    Add findMaxLength function in Solution class
    Implement findMaxLength function to find the maximum length of a contiguous subarray with equal number of 0s and 1s.

---
+++ solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int,int>count;
+        count[0]=-1;
+        int sum=0;
+        int maxlen=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=(nums[i]==0)?-1:1;
+            if(count.find(sum)!=count.end()){
+                maxlen=max(maxlen,i-count[sum]);
+            }else
+                count[sum]=i;
+        }
+        return maxlen;
+    }
+};
